1c9c17ab03db1291e001f857cdf296befcbfc818
Joseph DeGuzman <josephdeguzman@Josephs-MBP.fios-router.home>
2021-04-09 01:31:29 -0400

    added first test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,36 @@
 const request = require("supertest");
-    const app = require("../index");
+const app = require("../index");
+const chai = require('chai');
+const expect = chai.expect;
+var should = chai.should()
+
 
     describe("GET /", () => {
       it("respond with Hello World", (done) => {
         request(app).get("/").expect("Hello World", done);
       })
+    });
+
+    describe('GraphQLGetUser', () => {
+      it('Returns user with username = "jacknewuser', (done) => {
+          request(app).post('/graphql')
+          .send({ query: ` query
+          { getUser(username: "jacknewuser") 
+            { name 
+              username 
+              email 
+            } 
+          }`})
+          .expect(200)
+          .end((err,res) => {
+              res.body.data.getUser.should.have.property('name');
+              expect(res.body.data.getUser.name).to.equal('JACK');
+              res.body.data.getUser.should.have.property('username');
+              expect(res.body.data.getUser.username).to.equal('jacknewuser');
+              res.body.data.getUser.should.have.property('email');
+              expect(res.body.data.getUser.email).to.equal('jack123@gmail.com');
+              if (err) return done(err);
+              done();
+            })
+       })
     });
