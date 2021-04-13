f44aa82a8b71d6d466c8e7283b736f218174f21d
Joseph DeGuzman <josephdeguzman@Josephs-MBP.fios-router.home>
2021-04-12 22:49:52 -0400

    added navbar component and created pages for Home, Login, and SignUp

--- brainiac/src/App.js
+++ brainiac/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Route exact path = '/' component = {HomePage}/>
+      <Route exact path = '/login' component = {LoginPage}/>
+      <Route exact path = '/login' component = {SignUpPage}/>
+    </Router>
   );
 }
 
--- brainiac/src/index.js
+++ brainiac/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ApolloProvider from './ApolloProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(ApolloProvider, document.getElementById('root'));
 
